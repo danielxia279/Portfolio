72502ab7d067458addb343610ab9c8817eaf4f0b
Daniel Xia <daniel.xia.279@gmail.com>
2019-07-19 19:49:21 -0400

    changed nodemailer auth

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 
 const port = 5000;
 
-require('dotenv').config();
+require('dotenv').config({ path:__dirname+'/./../.env' });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -25,14 +25,14 @@ app.post('/send', (req,res) => {
     service: 'gmail',
     port: 465,
     auth: {
-      user: process.env.USER,
-      pass: process.env.PASS,
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_PASS,
     }
   });
 
   var mailOptions = {
     from: data.dataEmail,
-    to: 'captchafailingrobotjuice@gmail.com',
+    to: 'daniel.xia.279@gmail.com',
     subject: 'Portfolio Contact',
     html: `<p>${data.dataName}</p>
             <p>${data.dataEmail}</p>
